53e2de78d01e807399689c82594ade259eeaaced
Michael Redig <mredig@gmail.com>
2020-05-18 16:09:00 -0500

    (feat) added test confirming uiwindow extension working

--- Tests/TouchDollTests/TouchDollTests.swift
+++ Tests/TouchDollTests/TouchDollTests.swift
@@ -1,14 +1,38 @@
+import UIKit
 import XCTest
 @testable import TouchDoll
 
 final class TouchDollTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-    }
+	func testWindowExtension() {
+		let window = UIWindow()
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+		window.gestureRecognizers?.forEach {
+			let theType = type(of: $0)
+			XCTAssertFalse(TouchCatcher.self == theType)
+		}
+
+		XCTAssertEqual(window.layer.sublayers, nil)
+
+		window.showTouches()
+
+		let hasCatcher = window.gestureRecognizers?.contains(where: { TouchCatcher.self == type(of: $0) })
+		XCTAssertTrue(hasCatcher == true)
+
+		let hasShapeLayer = window.layer.sublayers?.contains(where: { CAShapeLayer.self == type(of: $0) })
+		XCTAssertTrue(hasShapeLayer == true)
+		XCTAssertEqual(window.layer.sublayers?.count, 1)
+
+		window.hideTouches()
+
+		window.gestureRecognizers?.forEach {
+			let theType = type(of: $0)
+			XCTAssertFalse(TouchCatcher.self == theType)
+		}
+
+		XCTAssertEqual(window.layer.sublayers, nil)
+	}
+
+	static var allTests = [
+		("testWindowExtension", testWindowExtension),
+	]
 }
